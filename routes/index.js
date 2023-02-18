2e7b9c72d968cc7de0a358d13ec351a0f7bf7aa1
Atlas60229 <b1010989126@tmu.edu.tw>
2023-02-19 06:19:36 +0800

    新增login and register 頁面

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const users = require('./modules/users')
 
+
+router.use('/users', users)  // 將網址結構符合 /users 字串的 request 導向 todos 模組 
 router.use('/', home)       // 將網址結構符合 / 字串的 request 導向 home 模組 
 
 module.exports = router
